c0687f9eb25495b74f6a669af5cb04626fecb1eb
samatar abdirahman <samatarabdirahman12@gmail.com>
2025-11-24 18:03:59 +0300

    added main.js and index.html

---
+++ main.js
@@ -0,0 +1,61 @@
+// alert("Hi, There is alert")
+
+
+// vars 
+let magac = 'ali'
+console.log(magac) 
+
+let age = 20
+console.log(age)
+
+let isMaried = false
+console.log(isMaried)
+
+// let 1999 = '1990'
+
+let _magac = 'adan'
+console.log(_magac)
+
+let X =90
+let x = 10
+
+console.log(X)
+console.log(x)
+
+// let console = 'adan'
+// console.log(console)
+
+// arithmetic operators
+
+let a = 90
+let y = 10
+
+let z1 = a + y
+console.log(z1)
+
+let z2 = a - y
+console.log(z2)
+
+let z3 = a * 2
+console.log(z3)
+
+let z4 = a / 2
+console.log(z4)
+
+let s = 2
+let z5 = s ** 3
+console.log(z5) 
+
+
+let val = 10
+
+let newVal = 10 % 3
+console.log(newVal)
+
+
+let k = 5;
+k+=9;
+let z = k;
+console.log(z)
+
+console.log(2==8)
